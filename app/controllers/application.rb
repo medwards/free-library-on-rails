3a04385f32391ec6e871c56114ae175a54d66763
Michael Edwards <medwards@minister.(none)>
2008-04-14 18:42:02 -0600

    Ran rails init.

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '80917b8faf645ce57e19ede4fec57e60'
+end

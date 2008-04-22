0b005e07fcee65018564d34c03b958ca905a489f
Michael Edwards <medwards@minister.(none)>
2008-04-22 02:55:46 -0600

    added helper stub for a less vicious version of h(string)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+	def clean(input)
+		# XXX change this to a more fine-grained cleanup of the input and then untain the string
+		return h(input)
+	end
 end

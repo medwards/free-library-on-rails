49376d678754af895cdd211efebedac1ad7fa6b5
Brendan Taylor <whateley@gmail.com>
2008-08-02 21:15:34 -0600

    added a really crappy index page

--- config/routes.rb
+++ config/routes.rb
@@ -30,12 +30,12 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
+
+  # The Big GET /
+  map.root :controller => "welcome"
 
   # Install the default routes as the lowest priority.
   #   I try to avoid falling back on these -- bct

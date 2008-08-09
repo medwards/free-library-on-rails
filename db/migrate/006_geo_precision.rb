c81e150e76ff1e3348049f74f174c9517dcc9a3a
Michael Edwards <medwards@minister.walledcity.ca>
2008-08-09 13:31:48 -0600

    Fixed errors on a clean rake db:migrate

--- db/migrate/006_geo_precision.rb
+++ db/migrate/006_geo_precision.rb
@@ -1,8 +1,8 @@
 # integer latitudes and longitudes don't give very good precision
 class GeoPrecision < ActiveRecord::Migration
   def self.up
-    change_column :users, :latitude,  :decimal, :scale => 3, :null => false
-    change_column :users, :longitude, :decimal, :scale => 3, :null => false
+    change_column :users, :latitude,  :decimal, :scale => 3, :precision => 6, :null => false
+    change_column :users, :longitude, :decimal, :scale => 3, :precision => 6, :null => false
   end
 
   def self.down

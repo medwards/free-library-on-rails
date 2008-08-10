8b74297bd630a7a27f3a73bcc41a270c50993bc8
Brendan Taylor <whateley@gmail.com>
2008-08-10 15:32:51 -0600

    added list of tags, made list of tags on item page nicer

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,8 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.find :all, :order => 'name'
+  end
+
   def show
     @tag = Tag.find_by_name(params[:id])
     @items = @tag.items

ca3a1cd8f9176a086a9248e28b412ba1c46874a2
Brendan Taylor <whateley@gmail.com>
2008-09-06 00:08:36 -0600

    separated out tag editor javascript

---
+++ public/javascripts/tageditor.js
@@ -0,0 +1,62 @@
+function addTag(tag) {
+	if(tag == "")
+		return;
+
+	var selTags = selectedTags();
+	if(selTags.indexOf(tag) > -1)
+		// we've already selected that tag
+		return;
+
+	selTags.push(tag);
+
+	$("tags").setValue(selTags.join(","));
+
+	listTag(tag);
+}
+
+function removeTag(tag) {
+	var removed = selectedTags().without(tag).join(",")
+
+	$("tags").setValue(removed)
+}
+
+function selectedTags() {
+	return $("tags").getValue().split(",");
+}
+
+function listTag(tag) {
+	var removeTagEl = new Element("span", { 'class': 'remove-tag' }).update("×");
+
+	removeTagEl.observe('click', function() {
+		removeTag(tag);
+		li.remove();
+	});
+
+	var li = new Element("li").update(removeTagEl);
+	li.insert(tag);
+
+	$("tagList").insert(li);
+}
+
+Event.observe(window, 'load', function() {
+	$("tags").hide();
+
+	var tagList = new Element("ul", { 'id': 'tagList' });
+
+	var newTag = new Element("input");
+	var addTagEl = new Element("span", { 'class': 'add-tag' }).update("+");
+
+	$("tags").up().insert(newTag);
+	$("tags").up().insert(addTagEl);
+	$("tags").up().insert(tagList);
+
+	addTagEl.observe('click', function() {
+		addTag($(newTag).getValue());
+		$(newTag).clear();
+	});
+
+	var selTags = selectedTags();
+	for(var i = 0; i < selTags.length; i++) {
+		listTag(selTags[i]);
+	}
+});

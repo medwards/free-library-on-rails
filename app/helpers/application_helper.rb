9296b42d3da988aa2fcfd42534afd08b6488e0c0
Michael Edwards <medwards@pilgrim.walledcity.ca>
2008-12-26 12:59:27 -0700

    Added GNU Affero GPL to the application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,20 @@
+# Copyright 2009 Michael Edwards, Brendan Taylor
+# This file is part of free-library-on-rails.
+# 
+# free-library-on-rails is free software: you can redistribute it
+# and/or modify it under the terms of the GNU Affero General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+
+# free-library-on-rails is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public
+# License along with free-library-on-rails.
+# If not, see <http://www.gnu.org/licenses/>.
+
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 	def clean(input)

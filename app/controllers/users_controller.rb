799f15875208a791a01199609afa615efadd975e
Michael Edwards <medwards@pilgrim.walledcity.ca>
2008-12-26 12:21:41 -0700

    Added pagination to user collections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find_by_login(params[:id])
+		@items = @user.owned.paginate(:all, :page => params[:page], :order => 'title')
 
 		four_oh_four unless @user
 	end

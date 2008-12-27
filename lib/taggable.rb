30696930c4725eb1ce533d2cd1d29485e71e7258
Brendan Taylor <whateley@gmail.com>
2008-12-27 12:38:50 -0700

    another AGPL header, method for adding single tags

--- lib/taggable.rb
+++ lib/taggable.rb
@@ -1,4 +1,21 @@
-# to use this:
+# Copyright 2009 Michael Edwards, Brendan Taylor
+# This file is part of free-library-on-rails.
+#
+# free-library-on-rails is free software: you can redistribute it
+# and/or modify it under the terms of the GNU Affero General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+
+# free-library-on-rails is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with free-library-on-rails.
+# If not, see <http://www.gnu.org/licenses/>.
+
+# to make a model taggable:
 #
 # 1. a XXXXTagging model needs to be defined, see ItemTagging or UserTagging
 # 2. in the model you want to be taggable you need to define an class method named tagging_class:
@@ -37,13 +54,18 @@ module Taggable
 		end
 
 		tags.uniq.each do |tag|
-			if not self.tags.member? tag
-				tagging = self.class.tagging_class.new
-				tagging.tag = Tag.find_or_create_by_name tag
-				tagging.thing = self
+			add_tag tag
+		end
+	end
 
-				self.taggings << tagging
-			end
+	# add a single String to this thing's tags
+	def add_tag tag
+		if not self.tags.member? tag
+			tagging = self.class.tagging_class.new
+			tagging.tag = Tag.find_or_create_by_name tag
+			tagging.thing = self
+
+			self.taggings << tagging
 		end
 	end
 

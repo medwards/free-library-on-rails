c077d572833f8b73d279792b0b7548ff4a2ed7cd
Brendan Taylor <whateley@gmail.com>
2009-01-16 19:50:32 -0700

    sort 'tags' page by count of times the tag was used

--- app/models/item_tagging.rb
+++ app/models/item_tagging.rb
@@ -1,6 +1,6 @@
 # Copyright 2009 Michael Edwards, Brendan Taylor
 # This file is part of free-library-on-rails.
-# 
+#
 # free-library-on-rails is free software: you can redistribute it
 # and/or modify it under the terms of the GNU Affero General Public
 # License as published by the Free Software Foundation, either
@@ -10,7 +10,7 @@
 # useful, but WITHOUT ANY WARRANTY; without even the implied
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public
 # License along with free-library-on-rails.
 # If not, see <http://www.gnu.org/licenses/>.
@@ -20,4 +20,10 @@ class ItemTagging < ActiveRecord::Base
 	belongs_to :tag
 
 	def to_s; tag.name; end
+
+	def self.counts
+		ItemTagging.count(:include => [:tag],
+						 :group => 'name',
+						 :order => 'COUNT(*) DESC, name ASC')
+	end
 end

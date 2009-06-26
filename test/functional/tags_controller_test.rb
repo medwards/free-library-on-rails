3a255c28231a06be9e653ba71d27a5490e3b0324
Brendan Taylor <whateley@gmail.com>
2009-06-25 21:26:54 -0600

    add tags controller test

---
+++ test/functional/tags_controller_test.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class TagsControllerTest < ActionController::TestCase
+	def setup
+		@controller = TagsController.new
+		@request    = ActionController::TestRequest.new
+		@response   = ActionController::TestResponse.new
+	end
+
+	def test_routing
+		assert_routing 'tags', :controller => 'tags', :action => 'index'
+		assert_routing 'tags/politics', :controller => 'tags', :action => 'show', :id => 'politics'
+	end
+
+	def test_index
+		get :index, :id => items(:lhd)
+		assert_response :success
+
+		assert_match /politics/, @response.body
+		assert_match /spain/, @response.body
+	end
+
+	def test_show
+		get :show, :id => 'politics'
+		assert_response :success
+
+		assert_match /Homage to Catalonia/, @response.body
+		assert_match /Soylent Green/, @response.body
+	end
+end

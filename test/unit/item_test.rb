e25fe2084c387a7b7e1104779361bba6d80a835b
Brendan Taylor <whateley@gmail.com>
2009-09-05 05:56:51 -0600

    retabbed item_test.rb

--- test/unit/item_test.rb
+++ test/unit/item_test.rb
@@ -1,46 +1,44 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  def test_owner
-    lhd = items(:lhd)
-    medwards = users(:medwards)
+	def test_owner
+		lhd = items(:lhd)
+		medwards = users(:medwards)
 
-    assert_equal medwards, lhd.owner
-  end
+		assert_equal medwards, lhd.owner
+	end
 
-  def test_tagging
-    htc = items(:htc)
+	def test_basic_tagging
+		htc = items(:htc)
 
-    tags = htc.tags.sort
+		tags = htc.tags.sort
+		assert_equal ['politics', 'spain'], tags
 
-    assert_equal ['politics', 'spain'], tags
+		htc.tag_with ['nonfiction']
 
-    # tags can be added
-    htc.tag_with ['nonfiction']
+		# items can be found by tag
+		tagged_with = Item.find_by_tag('nonfiction')
 
-    # items can be found by tag
-    tagged_with = Item.find_by_tag('nonfiction')
+		assert_equal 1, tagged_with.length
+		assert_equal htc, tagged_with[0]
+	end
 
-    assert_equal 1, tagged_with.length
-    assert_equal htc, tagged_with[0]
-  end
+	def test_current_loan
+		lhd = items(:lhd)
+		bct = users(:bct)
 
-  def test_current_loan
-    lhd = items(:lhd)
-    bct = users(:bct)
+		assert lhd.loaned?
+		assert_equal bct, lhd.borrower
+	end
 
-    assert lhd.loaned?
-    assert_equal bct, lhd.borrower
-  end
+	def test_return_item
+		lhd = items(:lhd)
 
-  def test_return_item
-    lhd = items(:lhd)
+		lhd.returned!
 
-    lhd.returned!
+		assert !lhd.loaned?
+		assert_nil lhd.borrower
 
-    assert !lhd.loaned?
-    assert_nil lhd.borrower
-
-    assert Loan.find(:all, :conditions => {:item_id => lhd, :status => 'lent'}).empty?
-  end
+		assert Loan.find(:all, :conditions => {:item_id => lhd, :status => 'lent'}).empty?
+	end
 end

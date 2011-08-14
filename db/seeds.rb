15417a3b7dde017805527337a5386ef558f192be
Brendan Taylor <whateley@gmail.com>
2011-08-14 18:04:58 +0000

    add the 'edmonton' region to db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Region.create :name => 'Edmonton', :subdomain => 'edmonton'

4eb6842497cbd3de415963cd4628e272a4a75846
wvengen <dev-rails@willem.engen.nl>
2014-10-31 15:15:13 +0100

    make adding a new book work again

--- test/unit/book_test.rb
+++ test/unit/book_test.rb
@@ -4,7 +4,7 @@ class BookTest < ActiveSupport::TestCase
   def test_new_from_isbn
     shne = Book.new_from_isbn('0-385-66004-9')
 
-    assert_equal '0385660049', shne.isbn
+    assert_equal '0385660049', shne.isbn.to_s
     assert_equal 'A Short History of Nearly Everything', shne.title
     assert_equal 'Bryson', shne.author_last
     assert_equal 'Bill', shne.author_first
